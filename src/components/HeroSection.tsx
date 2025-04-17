93c618a1e67c60527de609848fc438c6757a17ac
Md. Ahsanul Karim <ahsan.arnob.92@gmail.com>
2025-04-18 03:46:48 +0600

    chnages on hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -27,14 +27,19 @@ export default function HeroSection() {
                 </div>
                 <div className="w-full  flex items-center order-2 md:order-1" style={{ background: '#ECECDC' }} data-aos="fade-right">
                     <div className="w-full px-8 py-5 md:px-12 lg:px-16">
-                        <h1 className="md:mb-3 leading-tight text-2xl sm:text-2xl md:text-3xl lg:text-5xl pt-4 pb-4">
+                        <h1 className="md:mb-3 leading-tight text-[20px] sm:text-2xl md:text-3xl lg:text-5xl pt-4 pb-4">
                             Transformez votre intérieur <br />
                             en un lieu qui vous ressemble.
                         </h1>
                         <p className="text-base sm:text-base md:text-base lg:text-2xl mb-6 pt-4 pb-4">
-                            Nous serions ravis d'échanger sur votre projet, <br />
-                            Obtenez une estimation en moins de 2 minutes.
-                        </p>
+  <span className="block sm:hidden">
+    Estimation en moins de 2 minutes.
+  </span>
+  <span className="hidden sm:block">
+    Nous serions ravis d'échanger sur votre projet, <br />
+    Obtenez une estimation en moins de 2 minutes.
+  </span>
+</p>
                         <div className="flex flex-row gap-3 justify-center md:justify-start">
                             <Button asChild variant="outline" className="border-black border-2 text-black hover:bg-black hover:text-white py-5 px-8 rounded-none">
                                 <Link href="#quickquote">Devis Gratuit</Link>
